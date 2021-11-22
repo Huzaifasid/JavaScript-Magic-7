a9c7b68bb89e472212c0ed30bb006ee4b0685304
Huzaifa Siddiqui <siddiquihuzaifa68@gmail.com>
2021-11-22 22:28:41 +0500

    Assignment

---
+++ Chap 43-48/app.js
@@ -0,0 +1,80 @@
+// Question 1
+
+function alertLink() {
+    var link = document.getElementById("Alert");
+    link= alert("You Click on the Link");
+}
+
+// Question 2
+
+
+function iphone() {
+    var i = document.getElementById("Iphone");
+   i= alert("Thanks for buying Iphone");
+}
+
+function samsung() {
+    var sam = document.getElementById("Samsung");
+   sam= alert("Thanks for buying Samsung");
+}
+
+function motorola() {
+    var moto = document.getElementById("Motorola");
+    moto= alert("Thanks for buying Motorola");
+}
+
+function realme() {
+    var real = document.getElementById("RealME");
+  real= alert("Thanks for buying Real Me");
+}
+
+
+// Question 3
+
+function deletE() {
+    var deletE = document.getElementById("table")
+    deletE.className = "tablehide"
+
+}
+function deletE2() {
+    var deletE = document.getElementById("table2")
+    deletE.className = "tablehide"
+
+}
+function deletE3() {
+    var deletE = document.getElementById("table3")
+    deletE.className = "tablehide"
+
+}
+function deletE4() {
+    var deletE = document.getElementById("table4")
+    deletE.className = "tablehide"
+}
+
+// Question 4
+
+
+function imagehover() {
+    var hover = document.getElementById("hover");
+    hover.src = "images/pizza.jpg"
+
+
+}
+function imageOut() {
+    var hover = document.getElementById("hover");
+    hover.src = "./images/burger img.jpg"
+
+}
+
+
+
+
+// function counter() {
+//     var count = 0;
+//     document.getElementById("timer")
+//     for(var i =1; i>count; i++)
+//     {
+//         count++
+//     }
+//    console.log(count)
+// }
---
+++ Chap 49-52/app.js
@@ -0,0 +1,34 @@
+function userForm() {
+    var formName = document.getElementById("Name");
+    var useremail = document.getElementById("Email");
+    document.write("<br>Congratulations You Have Signed up Succesfully<br>")
+    document.write("<br>Name: " + formName.value)
+    document.write("<br>Your Email: " + useremail.value)
+}
+var para = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae earum tempora corrupti ad sequi deserunt, voluptates aut, ipsum animi maxime nostrum debitis ipsa alias accusamus  reiciendis obcaecati soluta id expedita."
+function fullP() {
+    var shortpara = document.getElementById("shortpara");
+    shortpara.innerHTML = para
+}
+
+
+
+// Question 3
+
+// function edittext() {
+//     var text = document.getElementById("userName")
+//     var userText = prompt("Enter Your Name");
+ 
+
+// }
+
+
+
+
+
+
+
+
+
+
+
